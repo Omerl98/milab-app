3d14f6fa15187c5a4f512eaa5525e0c7ab3de8f9
Omer Lehrer <Omer.Lehrer98@gmail.com>
2022-04-06 09:34:47 +0300

    small changes in login.js

--- src/login.js
+++ src/login.js
@@ -3,11 +3,17 @@ import appleIcon from "./assets/apple.svg";
 import googleIcon from "./assets/google.svg";
 import fbIcon from "./assets/facebook.svg";
 import mainLogo from "./assets/mainLogo.png";
+import {useEffect, useState} from "react";
 
 
 
 
 function Login() {
+
+    const [firstName,setFirstName] = useState('');
+    const [lastName,setLastName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
 
 
     return (
@@ -18,18 +24,18 @@ function Login() {
 
             <form className="login-form">
                 <label>First Name</label>
-                <input type="text" name="first-name"/>
+                <input type="text" onChange={event => setFirstName(event.target.value)} name="first-name"/>
                 <label>Last Name</label>
-                <input type="text" name="last-name"/>
+                <input type="text" onChange={event => setLastName(event.target.value)} name="last-name"/>
                 <label>Email Address</label>
-                <input type="text" name="email"/>
+                <input type="text" onChange={event => setEmail(event.target.value)} name="email"/>
                 <label>Password</label>
-                <input type="text" name="password"/>
+                <input type="text" onChange={event => setPassword(event.target.value)} name="password"/>
                 <button class="submit-button" type="submit">Join the hood</button>
             </form>
             <p className="line-text"><span>OR</span></p>
             <div className="social-nav">
-                <img src={fbIcon} className="icon fb-icon" />
+                <img src={fbIcon} href="#" className="icon fb-icon" />
                 <img src={googleIcon} className="icon google-icon" />
                 <img src={appleIcon} className="icon apple-icon" />
             </div>
